00a64fbda8ddce3aad416250c89a2590fc4ff0a5
Yuri Kholodkov <kyurivlis@autistici.org>
2024-09-28 14:05:49 +0300

    smoother debian install
    It works!

--- deb/consfig.lisp
+++ deb/consfig.lisp
@@ -2,7 +2,7 @@
 (in-consfig "ouah.ml.consfig")
 (named-readtables:in-readtable :consfigurator)
 
-(defhost 192.168.1.169
+(defhost 192.168.1.64
     (:deploy ((:ssh :user "kyurivlis") :sudo :sbcl))
   "Work VM"
   (os:debian-stable "bookworm" :amd64)
