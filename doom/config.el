649bee3b5455994e6961b3d410656f4e92008ff9
Yuri Kholodkov <kyurivlis@autistici.org>
2024-10-24 09:57:29 +0300

    we're doomed!

---
+++ doom/config.el
@@ -0,0 +1,25 @@
+;;; config.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024
+;;
+;; Author:  <kyurivlis@nl>
+;; Maintainer:  <kyurivlis@nl>
+;; Created: октября 24, 2024
+;; Modified: октября 24, 2024
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/kyurivlis/config
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'config)
+;;; config.el ends here
