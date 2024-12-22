3916993c1b96070704ad49074351b87d24a85e41
Yuri Kholodkov <kyurivlis@autistici.org>
2024-12-22 17:33:52 +0300

    yeh well

--- rde/init.sh
+++ rde/init.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env sh
+#!/bin/sh
+herd stop guix-daemon
+guix-daemon --substitute-urls='https://bordeaux.guix.gnu.org http://ci.guix.trop.in' --discover=yes &
 
 guix shell git just direnv -- \
     git clone https://github.com/kyurivlis/config; \
     cd config/rde; \
     mkdir -p ~/.config/guix; \
-    echo /tmp/config/rde >> ~/.config/guix/shell-authorized-directories; \
     direnv allow; \
-    guix shell
+    guix shell -m manifest.scm
